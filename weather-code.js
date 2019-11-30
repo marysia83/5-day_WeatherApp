f33b2db7ca00c5c2ac32e60f6c7045479922c440
Maria Galent <mariagalent@gmail.com>
2019-11-30 01:24:48 -0600

    Pseudo-code in JS

--- weather-code.js
+++ weather-code.js
@@ -1,8 +1,16 @@
-var form = document.querySelector("form");
+var form = document.querySelector("form-group");
+var ul = document.querySelector("ul");
+var button = document.querySelector("button");
+var input = document.getElementById("searchCity");
 
 
 
-//URL that we need to query the database:
+//URL to query the database:
 var APIKey = "70ef7114ad41776899346875cbf45412";
 var searchCity = document.getElementById("searchCity");
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "units=imperial&appid=" + APIKey;
+
+
+//1. user types a city, clicks search. Data goes to local storage
+//2. searched city will appear in a list of recent searches as a link
+//3. searched city will go to API and show the results as 5 day forecast
