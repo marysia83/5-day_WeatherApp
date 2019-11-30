f2440f5cef47e6c98a4bf55c83433a7ac3ede5e2
Maria Galent <mariagalent@gmail.com>
2019-11-30 14:39:22 -0600

    Created fucntion for local stogare and eventlistener for search button

--- weather-code.js
+++ weather-code.js
@@ -11,6 +11,28 @@ var searchCity = document.getElementById("searchCity");
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "units=imperial&appid=" + APIKey;
 
 
-//1. user types a city, clicks search. Data goes to local storage
-//2. searched city will appear in a list of recent searches as a link
-//3. searched city will go to API and show the results as 5 day forecast
+//1. User types a city name, clicks search. Data goes to local storage:
+
+localStorage.setItem("items", JSON.stringify(itemsArray));
+
+var data = JSON.parse(localStorage.getItem("items"));
+
+function liMaker(text) {
+    var li = document.createElement("li");
+    li.textContent = text;
+    ul.appendChild(li);
+};
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault()
+
+    itemsArray.push(input.value)
+    input.value = ""
+});
+
+
+
+
+//2. searched city will go to API and show the results as 5 day forecast
+//3. searched city will appear in a list of recent searches as a link 
+//4. when recent searches are clicked, it get to #2
